5aa4089af91f9dff05c0abe826a80face8dcecc8
Aneesh <54546961+gargAneesh@users.noreply.github.com>
2024-11-13 21:33:02 -0800

    Update ucsbece154a_controller.v

--- ucsbece154a_controller.v
+++ ucsbece154a_controller.v
@@ -34,7 +34,7 @@ always @ * begin
         instr_lw_op:       controls = 12'b1_000_1_0_01_0_00_0; // Load Word
         instr_sw_op:       controls = 12'b0_001_1_1_xx_0_00_0; // Store Word
         instr_Rtype_op:    controls = 12'b1_xxx_0_0_00_0_10_0; // R-type ALU Operations
-        instr_beq_op:      controls = 12'b0_010_0_0_xx_1_01_0; // Branch Equal
+        instr_beq_op:      controls = 12'b0_010_0_0_xx_1_01_0; // Branch Equal (set ImmSrc to 010)
         instr_ItypeALU_op: controls = 12'b1_000_1_0_00_0_10_0; // I-type ALU Operations
         instr_jal_op:      controls = 12'b1_011_x_0_10_x_xx_1;  // JAL
         instr_lui_op:      controls = 12'b1_100_1_0_11_0_10_0; // LUI
